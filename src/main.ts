7f64ca7a23b87dd4c208f586044d157e6aba05b9
cdlavacudeg <cristianlav10@gmail.com>
2024-02-11 10:06:20 -0500

    [Add] Swagger configuration
    Swagger for endpoint documentation and testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  const swaggerDocumentConfig = new DocumentBuilder().setTitle('Red-Social API').setVersion('1.0').build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerDocumentConfig);
+
+  SwaggerModule.setup('/documentation', app, swaggerDocument);
   await app.listen(process.env.API_PORT ? parseInt(process.env.API_PORT) : 3000);
 }
 bootstrap();
